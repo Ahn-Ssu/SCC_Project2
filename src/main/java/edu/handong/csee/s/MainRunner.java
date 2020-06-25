f00b817a743bb8fb07870ed0199a2d66df98b4d0
suhyun <suhyun@172.17.218.42>
2020-06-25 12:27:24 +0900

    created Main file

---
+++ src/main/java/edu/handong/csee/s/MainRunner.java
@@ -0,0 +1,16 @@
+package edu.handong.csee.s;
+
+public class MainRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MainRunner myRunner = new MainRunner();
+		myRunner.run(args);
+	}
+
+	private void run(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
